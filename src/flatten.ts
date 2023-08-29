7b49dacedd35c54f963dc034f95f5ced8fabeb6e
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-08-29 02:33:46 +0200

    fix: deps

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,42 +1,11 @@
-import { Block } from "comment-parser";
 import { Parser } from "yaml";
 import {
   BlockMap,
-  CollectionItem,
   SourceToken,
   Document,
   Token,
   BlockSequence,
 } from "yaml/dist/parse/cst";
-
-const yaml = `# family stuff
-# blabla
-family:
-  cities:
-    - Panama
-    - Madrid
-  # @param {object} mother The mother
-  mother:
-    # @param {string} name The mother's name
-    name:
-    # @param {object} [mother] The mother's mother
-    mother:
-      # @param {string} [name] The mother mother's name
-      name:
-`;
-
-// const spaceBeforeDocument =
-//   tokens
-//     .slice(documentIndex - 2, documentIndex)
-//     .filter((t) => t.type === "newline").length > 1;
-
-// const comments = {
-//   headComments: tokens
-//     .slice(0, documentIndex)
-//     .filter((t) => t.type === "comment"),
-//   spaceBeforeDocument,
-//   documentIndex,
-// };
 
 export const flattenBlock = (blockMap: BlockMap | BlockSequence) => {
   const nodes: (SourceToken | Token)[] = [];
@@ -84,39 +53,3 @@ export const flattenYaml = (yaml: string) => {
   ];
   return nodes;
 };
-
-// let comments: SourceToken[] = [];
-// let lineCount = 0;
-// const scalars: any[] = [];
-// nodes.forEach((node) => {
-//   if (node.type === "comment") {
-//     comments.push(node);
-//   }
-//   if (node.type === "scalar") {
-//     scalars.push({
-//       ...node,
-//       comments,
-//     });
-//     comments = [];
-//   }
-// });
-
-// blockMap.items.forEach((item) => {
-//   nodes.push(item.key);
-//   if (item.start) {
-//     item.sep.forEach((sep) => {
-//       nodes.push(sep);
-//     });
-//   }
-//   if (item.sep) {
-//     item.sep.forEach((sep) => {
-//       nodes.push(sep);
-//     });
-//   }
-//   nodes.push(item.value);
-// });
-
-//#console.log(JSON.stringify(nodes, null, 2));
-//console.log(JSON.stringify(scalars, null, 2));
-
-//console.log(comments);
